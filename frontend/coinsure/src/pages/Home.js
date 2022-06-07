c0fca1454410caee07ace097c358ad3019ef7cf6
Jessica Arruda <sicarruda@gmail.com>
2022-06-07 14:29:19 -0300

    doc: esqueleto das paginas finalizado

--- frontend/coinsure/src/pages/Home.js
+++ frontend/coinsure/src/pages/Home.js
@@ -1,5 +1,6 @@
 // import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Cards from '../components/Cards'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
@@ -9,7 +10,9 @@ function Home() {
       <Link to="/userPage"> Pagina do usuario </Link>
       <Link to="/login"> Login </Link>
       <Link to="/Registration"> Cadastro </Link>
-
+      <div>
+        <Cards />
+      </div>
     </div>
   );
 }
--- frontend/coinsure/src/pages/Login.js
+++ frontend/coinsure/src/pages/Login.js
@@ -1,22 +1,48 @@
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Login() {
-  return(
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const chackEmailAndPassword = (email, password) => {
+    const validEmail = /^[a-zA-Z]+@[a-zA-Z]+\.[com]{3,}$/i;
+    const passwordLength = 5;
+    return validEmail.test(email) && password.length > passwordLength;
+  };
+
+  return (
     <div>
       <h1>Login</h1>
-      <label htmlFor='email-input'>
+      <label htmlFor="email-input">
         Email
-        <input type='email'></input>
+        <input
+          type="email"
+          id="email-input"
+          onChange={({ target }) => setEmail(target.value)}
+        ></input>
       </label>
-      <label htmlFor='password-input'>
+      <label htmlFor="password-input">
         Senha
-        <input type='password'></input>
+        <input
+          type="password"
+          id="password-imput"
+          onChange={({ target }) => setPassword(target.value)}
+        ></input>
       </label>
-      <button type="button"> Enviar </button>
+      <Link to="/">
+        <button
+          type="button"
+          disabled={
+            email && password ? !chackEmailAndPassword(email, password) : true
+          }
+        >
+          Enviar
+        </button>
+      </Link>
     </div>
-  )
+  );
 }
 
 export default Login;
