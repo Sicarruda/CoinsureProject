b16286d64e1d64e216a541503c215828bd846d47
Jessica Arruda <sicarruda@gmail.com>
2022-06-12 19:22:44 -0300

    estilo adicionado

--- frontend/coinsure/src/pages/Home.js
+++ frontend/coinsure/src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Cards from '../components/Cards';
 import getProductList from '../services/home';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
   const [objData, setObjData] = useState([]);
@@ -16,33 +16,40 @@ function Home() {
   }, []);
 
   return (
-    <div>
-      <h1>Olá aqui é Ipssis Literis</h1>
-
-      <Link to="/userPage"> Adicionar Produto </Link>
-      <Link to="/login"> Login </Link>
-      <Link to="/Registration"> Cadastro </Link>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus
-        venenatis molestie. Nulla aliquet hendrerit odio eget cursus. Vestibulum
-        pretium tincidunt tristique. Nam dictum massa odio, tincidunt gravida
-        nunc egestas a. Aliquam a nulla aliquam, finibus massa at, condimentum
-        velit. Vestibulum viverra hendrerit volutpat. Morbi nec justo mi. Aenean
-        molestie vestibulum neque. Morbi interdum at mi vitae fringilla. Fusce
-        fringilla eleifend vehicula. Ut sit amet congue diam. Duis nec malesuada
-        sem. Nullam fringilla porta quam, posuere faucibus neque. Etiam magna
-        sem, feugiat vel lectus quis, malesuada malesuada magna. Proin iaculis
-        risus posuere, faucibus nibh eget, sagittis arcu. Pellentesque pulvinar
-        arcu leo, egestas auctor lacus commodo a. Ut dapibus posuere libero nec
-        accumsan. Vestibulum varius mi ac varius convallis. Donec convallis,
-        elit quis pretium lacinia, elit risus ornare velit, sed elementum diam
-        arcu id tellus. Donec feugiat ante nec augue viverra, a porttitor justo
-        dictum. Aliquam pulvinar risus ultrices semper dignissim. Aenean tempor,
-        dui eu porttitor fringilla, tortor libero vulputate dolor, nec
-        sollicitudin mi ex vel nisl.
-      </p>
-      <h3>Esses são os nossos produtos!</h3>
+    <div class="container bg-light">
+      <h1 className='text-dark m-4'>Olá aqui é Ipssis Literis</h1>
+      <ul className="nav nav-pills nav-fill">
+        <li className="nav-item">
+          <Link className="nav-link " to="/userPage">
+            Adicionar Produto
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link " to="/login">
+            Login
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link " to="/Registration">
+            Cadastro
+          </Link>
+        </li>
+      </ul>
       <div>
+        <p className='text-secondary m-3 p-2'>
+          <strong>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus
+            venenatis molestie. Nulla aliquet hendrerit odio eget cursus.
+            Vestibulum pretium tincidunt tristique. Nam dictum massa odio,
+            tincidunt gravida nunc egestas a. Aliquam a nulla aliquam, finibus
+            massa at, condimentum velit. Vestibulum viverra hendrerit volutpat.
+            Morbi nec justo mi. Aenean molestie vestibulum neque. Morbi interdum
+            at mi vitae fringilla.
+          </strong>
+        </p>
+      </div>
+      <h3 className='m-3'>Esses são os nossos produtos!</h3>
+      <div className='row p-0 ml-3'>
         {objData.map((obj, index) => (
           <Cards key={index} objData={obj} />
         ))}
